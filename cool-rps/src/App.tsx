c926a389e5bbce455bea651623f2910b3bd35380
Luke Davies <lukedavies@inspiredthinking.group>
2025-10-06 15:46:59 +0100

    moved logic to new component

--- cool-rps/src/App.tsx
+++ cool-rps/src/App.tsx
@@ -1,8 +1,11 @@
 import { useState } from "react";
 import "./App.css";
 import Game from "./components/Game";
+import MovePicker from "./components/MovePicker";
+
 function App() {
   const [moveList, setMoveList] = useState<string[]>([
+    // loses to move + odd, beats move + even
     "rock",
     "paper",
     "scissors",
@@ -12,53 +15,21 @@ function App() {
   const [score, setScore] = useState<number>(0);
   const [playerMove, setPlayerMove] = useState<string>("");
   const [computerMove, setComputerMove] = useState<string>("");
-  function generateComputerMove() {
-    return moveList[Math.floor(Math.random() * moveList.length)];
-  }
 
-  // you LOSE to moves 1, 3, 5... ahead, WIN against 2, 4, 6... ahead
-  function getResult(playerMove: string, computerMove: string) {
-    if (playerMove === computerMove) return 0;
-    const n = moveList.length;
-    const playerIndex = moveList.indexOf(playerMove);
-    const computerIndex = moveList.indexOf(computerMove);
-    // circular distance around the array between 2 moves
-    const distance = (computerIndex - playerIndex + n) % n;
-    // if odd
-    if (distance % 2 === 1) {
-      setScore((prev) => prev - 1);
-      return -1; // lose
-      // if even
-    } else {
-      setScore((prev) => prev + 1);
-      return 1; // win
-    }
-  }
-  function playRound() {
-    const playerMove = prompt(`Type your move: ${moveList.join(", ")}`);
-
-    const computerMove = generateComputerMove();
-    const result = getResult(playerMove, computerMove);
-    alert(
-      `I chose ${computerMove}! Score change: ${result}. Current score: ${
-        score + result
-      }.`
-    );
-  }
   return (
     <div className="App">
       <h1>Rock Paper Scissors Spock Lizard</h1>
       <p>Score: {score}</p>
-      <button onClick={playRound}>Play Round</button>
       <Game
         moveList={moveList}
         playerMove={playerMove}
         setPlayerMove={setPlayerMove}
         computerMove={computerMove}
         setComputerMove={setComputerMove}
-        score={score}
         setScore={setScore}
       />
+      <MovePicker setMoveList={setMoveList} />
+      <img src="./diagram.webp" />
     </div>
   );
 }
--- cool-rps/src/components/Game.tsx
+++ cool-rps/src/components/Game.tsx
@@ -4,8 +4,51 @@ export default function Game({
   setPlayerMove,
   computerMove,
   setComputerMove,
-  score,
   setScore,
 }) {
-  return <p>hi</p>;
+  function generateComputerMove() {
+    return moveList[Math.floor(Math.random() * moveList.length)];
+  }
+
+  // you LOSE to moves 1, 3, 5... ahead, WIN against 2, 4, 6... ahead
+  function getResult(playerMove: string, computerMove: string) {
+    if (playerMove === computerMove) return 0;
+    const n = moveList.length;
+    const playerIndex = moveList.indexOf(playerMove);
+    const computerIndex = moveList.indexOf(computerMove);
+    // circular distance around the array between 2 moves
+    const distance = (computerIndex - playerIndex + n) % n;
+    // if odd
+    if (distance % 2 === 1) {
+      setScore((prev: number) => prev - 1);
+      return -1; // lose
+      // if even
+    } else {
+      setScore((prev: number) => prev + 1);
+      return 1; // win
+    }
+  }
+  function playRound(move: string) {
+    const compMove = generateComputerMove();
+    setPlayerMove(move);
+    setComputerMove(compMove);
+    getResult(move, compMove);
+  }
+  return (
+    <div>
+      <div>
+        {moveList.map((move) => (
+          <button key={move} onClick={() => playRound(move)}>
+            {move}
+          </button>
+        ))}
+      </div>
+      {playerMove && computerMove && (
+        <p>
+          u picked {playerMove}, i picked {""}
+          {computerMove}
+        </p>
+      )}
+    </div>
+  );
 }
