9c499edbb8c0e9ab7590fa2a1dce41548e155177
Luke Davies <lukedavies@inspiredthinking.group>
2025-10-13 15:30:47 +0100

    created healthbars for boss fight

--- cool-rps/src/components/BossFight.tsx
+++ cool-rps/src/components/BossFight.tsx
@@ -1,9 +1,13 @@
 import { useState, useEffect } from "react";
 import styles from "./BossFight.module.scss";
+import { getResult } from "../utils/getResult";
 
 export default function BossFight() {
-  const [healthbarWidth, setHealthbarWidth] = useState(150);
+  const [healthbarWidth, setHealthbarWidth] = useState(200);
+  const [playerHealth, setPlayerHealth] = useState(200);
   const [playerMove, setPlayerMove] = useState<string | null>(null);
+
+  const moveList = ["rock", "paper", "scissors"];
 
   useEffect(() => {
     const audio = new Audio("/BossMusic.mp3");
@@ -17,24 +21,47 @@ export default function BossFight() {
     width: healthbarWidth,
   };
 
+  const playerHealthStyle = {
+    width: playerHealth,
+  };
+
   console.log(playerMove);
+
+  function generateComputerMove() {
+    return moveList[Math.floor(Math.random() * moveList.length)];
+  }
+
+  function playRound(move: string) {
+    const compMove = generateComputerMove();
+    console.log(compMove, "compMove");
+    setPlayerMove(move);
+    const result = getResult(move, compMove, moveList);
+    console.log(result);
+    if (result === 1) {
+      setHealthbarWidth((prev) => prev - 50);
+    } else if (result === -1) {
+      setPlayerHealth((prev) => prev - 50);
+    }
+  }
 
   return (
     <div className={styles.container}>
+      <div className={styles.playerHealth} style={playerHealthStyle}>
+        LIFE
+      </div>
       <div className={styles.movesContainer}>
-        <button onClick={() => setHealthbarWidth((prev) => prev - 50)}></button>
         <img
-          onClick={() => setPlayerMove("rock")}
+          onClick={() => playRound("rock")}
           className={`${styles.icon} ${styles.rock}`}
           src="pixel-rock.png"
         ></img>
         <img
-          onClick={() => setPlayerMove("paper")}
+          onClick={() => playRound("paper")}
           className={`${styles.icon} ${styles.paper}`}
           src="pixel-paper.png"
         ></img>
         <img
-          onClick={() => setPlayerMove("scissors")}
+          onClick={() => playRound("scissors")}
           className={`${styles.icon} ${styles.scissors}`}
           src="pixel-scissors.png"
         ></img>
