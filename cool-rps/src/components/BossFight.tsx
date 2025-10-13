95902a77ee49a6f55647a95cac7ac7c4006dedde
Luke Davies <lukedavies@inspiredthinking.group>
2025-10-13 14:40:38 +0100

    started animations for bossfight choices, updated icons

--- cool-rps/src/components/BossFight.tsx
+++ cool-rps/src/components/BossFight.tsx
@@ -1,29 +1,48 @@
+import { useState, useEffect } from "react";
 import styles from "./BossFight.module.scss";
 
 export default function BossFight() {
-  const audio = new Audio("/BossMusic.mp3");
-  audio.volume = 0.1;
-  setTimeout(() => {
-    audio.play();
-  }, 1000);
+  const [healthbarWidth, setHealthbarWidth] = useState(150);
+  const [playerMove, setPlayerMove] = useState<string | null>(null);
+
+  useEffect(() => {
+    const audio = new Audio("/BossMusic.mp3");
+    audio.volume = 0.1;
+    setTimeout(() => {
+      audio.play();
+    }, 1000);
+  }, []);
+
+  const healthbarStyle = {
+    width: healthbarWidth,
+  };
+
+  console.log(playerMove);
 
   return (
     <div className={styles.container}>
       <div className={styles.movesContainer}>
+        <button onClick={() => setHealthbarWidth((prev) => prev - 50)}></button>
         <img
+          onClick={() => setPlayerMove("rock")}
           className={`${styles.icon} ${styles.rock}`}
           src="pixel-rock.png"
         ></img>
         <img
+          onClick={() => setPlayerMove("paper")}
           className={`${styles.icon} ${styles.paper}`}
           src="pixel-paper.png"
         ></img>
         <img
+          onClick={() => setPlayerMove("scissors")}
           className={`${styles.icon} ${styles.scissors}`}
           src="pixel-scissors.png"
         ></img>
       </div>
-      <img className={styles.boss} src="boss-pixel.png"></img>
+      <div className={styles.bossContainer}>
+        <div className={styles.healthbar} style={healthbarStyle}></div>
+        <img className={styles.boss} src="boss-pixel.png"></img>
+      </div>
     </div>
   );
 }
